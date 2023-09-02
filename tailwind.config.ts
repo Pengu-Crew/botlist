cf543ae61149c0d43e2cd18f9d24f760ea6bbe61
ElFietulais <pancongusanos@gmail.com>
2023-09-02 16:54:32 -0500

    basic welcome page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,17 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      'Gunmetal': '#253237',
+      'Charcoal': '#334146',
+      'OuterSpace': '#414F55',
+      'PayneGray': '#4F5D64',
+      'PaynesGray': '#5C6B73',
+      'CadetGray': '#7D909A',
+      'CadetsGray': '#9DB4C0',
+      'LightGray': '#C2DFE3',
+      'LightCyan': '#E0FBFC'
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
